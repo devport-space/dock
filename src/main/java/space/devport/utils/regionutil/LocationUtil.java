40e3d71e2276835d03b1d16692fb83380a420597
Wertik <jregvx@gmail.com>
2020-02-18 10:12:57 +0100

    LocationUtil, region save/load

---
+++ src/main/java/space/devport/utils/regionutil/LocationUtil.java
@@ -0,0 +1,49 @@
+package space.devport.utils.regionutil;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import space.devport.utils.DevportUtils;
+
+public class LocationUtil {
+
+    // Parse a location to string, uses ; as a delimiter.
+    public static String locationToString(Location location) {
+        return locationToString(location, ";");
+    }
+
+    // Parse a location to string, uses given delimiter
+    public static String locationToString(Location location, String delimiter) {
+
+        if (location == null) {
+            DevportUtils.inst.getConsoleOutput().err("Could not parse location to string, location is null.");
+            return null;
+        }
+
+        return location.getX() + delimiter + location.getY() + delimiter + location.getZ();
+    }
+
+    // Parse a location from string, uses ; as a delimiter
+    public static Location locationFromString(String dataString) {
+        return locationFromString(dataString, ";");
+    }
+
+    // Parse a location from string, uses given delimiter
+    public static Location locationFromString(String dataString, String delimiter) {
+        String[] arr = dataString.split(delimiter);
+
+        if (arr.length < 4) {
+            DevportUtils.inst.getConsoleOutput().err("Could not load a location from " + dataString + ", too few parameters.");
+            return null;
+        }
+
+        try {
+            return new Location(Bukkit.getWorld(arr[0]), Double.parseDouble(arr[1]), Double.parseDouble(arr[2]), Double.parseDouble(arr[3]));
+        } catch (NumberFormatException e) {
+            DevportUtils.inst.getConsoleOutput().err("Could not load a location from " + dataString + ", parameter not a number.");
+        } catch (NullPointerException e1) {
+            DevportUtils.inst.getConsoleOutput().err("Could not load a location from " + dataString + ", parameter(s) missing.");
+        }
+
+        return null;
+    }
+}
--- src/main/java/space/devport/utils/regionutil/Region.java
+++ src/main/java/space/devport/utils/regionutil/Region.java
@@ -3,6 +3,9 @@ package space.devport.utils.regionutil;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import space.devport.utils.DevportUtils;
 
 public class Region {
 
@@ -42,5 +45,36 @@ public class Region {
             return location.getX() <= max.getX() && location.getX() >= min.getX()
                     && location.getZ() <= max.getZ() && location.getZ() >= min.getZ()
                     && location.getY() <= max.getY() && location.getY() >= min.getY();
+    }
+
+    // Set region to yaml
+    public void set(FileConfiguration yaml, String path) {
+        ConfigurationSection section = yaml.createSection(path);
+
+        section.set("min", LocationUtil.locationToString(min));
+        section.set("max", LocationUtil.locationToString(max));
+    }
+
+    // Load region from yaml
+    public static Region loadRegion(FileConfiguration yaml, String path, String[] paths) {
+        Location min = LocationUtil.locationFromString(yaml.getString(path + "." + paths[0]));
+        Location max = LocationUtil.locationFromString(yaml.getString(path + "." + paths[1]));
+
+        if (min == null) {
+            DevportUtils.inst.getConsoleOutput().err("Could not load a region at path " + path + ", minimum location didn't load.");
+            return null;
+        }
+
+        if (max == null) {
+            DevportUtils.inst.getConsoleOutput().err("Could not load a region at path " + path + ", maximum location didn't load.");
+            return null;
+        }
+
+        return new Region(min, max, false);
+    }
+
+    public static Region loadRegion(FileConfiguration yaml, String path) {
+        String[] paths = new String[]{"min", "max"};
+        return loadRegion(yaml, path, paths);
     }
 }
