dd4ba42c66d5df53f92c8202608db0b3c9d62e45
Daniel Ciepluch <daniel.ciepluch@acd-czech.cz>
2020-03-02 19:32:32 +0100

    Basic reflection

---
+++ src/main/java/space/devport/utils/SpigotHelper.java
@@ -0,0 +1,27 @@
+package space.devport.utils;
+
+import lombok.Getter;
+import lombok.experimental.UtilityClass;
+import org.bukkit.Bukkit;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@UtilityClass
+public class SpigotHelper {
+
+    @Getter private static final boolean isPaper = Bukkit.getName().equalsIgnoreCase("paper");
+    @Getter private static final boolean isISpigot = Bukkit.getName().equalsIgnoreCase("ispigot");
+    //TODO: Add more spigot server checks
+
+    public static String extractNMSVersion() {
+        Matcher matcher = Pattern.compile("v\\d+_\\d+_R\\d+").matcher(Bukkit.getServer().getClass().getPackage().getName()); //Matches string v{INT}_{INT}_R{INT}
+        return matcher.find() ? matcher.group() : null;
+    }
+
+    public static String getVersion() {
+        String name = Bukkit.getServer().getClass().getPackage().getName();
+        String version = name.substring(name.lastIndexOf('.') + 1) + ".";
+        return version;
+    }
+}
