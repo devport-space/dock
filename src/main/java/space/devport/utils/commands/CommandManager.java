3784715d3a501351307fd5fdf6d6fd4f0427d67b
Wertík <jregvx@gmail.com>
2020-04-21 14:23:05 +0200

    Added a method for external command firing.

--- src/main/java/space/devport/utils/commands/CommandManager.java
+++ src/main/java/space/devport/utils/commands/CommandManager.java
@@ -1,13 +1,16 @@
 package space.devport.utils.commands;
 
+import com.google.common.base.Strings;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import space.devport.utils.DevportPlugin;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CommandManager implements CommandExecutor {
@@ -42,13 +45,29 @@ public class CommandManager implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
+        runCommands(sender, label, args);
+        return false;
+    }
+
+    private void runCommands(CommandSender sender, String label, String[] args) {
         for (MainCommand command : registeredCommands) {
             if (!command.getName().equalsIgnoreCase(label) && !command.getAliases().contains(label)) continue;
 
             command.runCommand(sender, label, args);
             break;
         }
+    }
 
-        return false;
+    /**
+     * Execute a registered command for a sender.
+     */
+    public void executeCommand(@Nullable CommandSender sender, @Nullable String command) {
+
+        if (Strings.isNullOrEmpty(command) || sender == null) return;
+
+        String[] arr = command.split(" ");
+        String label = arr[0].contains("/") ? arr[0].replace("/", "") : arr[0];
+        List<String> argList = new ArrayList<>(Arrays.asList(arr).subList(1, arr.length));
+        runCommands(sender, label, argList.toArray(new String[0]));
     }
 }
