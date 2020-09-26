052a69022edcfd2f835cb99c51c8230e2d9ec3e4
Wertik <jregvx@gmail.com>
2020-09-27 01:16:58 +0200

    Add vector parsing.

--- src/main/java/space/devport/utils/ParseUtil.java
+++ src/main/java/space/devport/utils/ParseUtil.java
@@ -1,6 +1,10 @@
 package space.devport.utils;
 
+import com.google.common.base.Strings;
 import lombok.experimental.UtilityClass;
+import org.bukkit.util.Vector;
+import org.jetbrains.annotations.Nullable;
+import space.devport.utils.item.Amount;
 
 @UtilityClass
 public class ParseUtil {
@@ -31,5 +35,27 @@ public class ParseUtil {
 
     public int parseInteger(String str, boolean... silent) {
         return parseInteger(str, 0, silent);
+    }
+
+    public Vector parseVector(@Nullable String str) {
+
+        if (Strings.isNullOrEmpty(str)) {
+            return new Vector();
+        }
+
+        String[] arr = str.split(";");
+
+        if (arr.length != 3) {
+            ConsoleOutput.getInstance().warn("Could not parse vector from " + str + ", invalid number of parameters.");
+            return new Vector();
+        }
+
+        Amount x = Amount.fromString(arr[0]);
+        Amount y = Amount.fromString(arr[1]);
+        Amount z = Amount.fromString(arr[2]);
+
+        return new Vector(x == null ? 0 : x.getDouble(),
+                y == null ? 0 : y.getDouble(),
+                z == null ? 0 : z.getDouble());
     }
 }
