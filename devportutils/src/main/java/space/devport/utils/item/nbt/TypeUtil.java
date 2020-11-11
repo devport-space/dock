7f762cf6764ccee14113cac4ee734a5c4e82b9dd
Wertik <jregvx@gmail.com>
2020-11-11 14:54:02 +0100

    Implement more appliers.

--- devportutils/src/main/java/space/devport/utils/item/nbt/TypeUtil.java
+++ devportutils/src/main/java/space/devport/utils/item/nbt/TypeUtil.java
@@ -1,6 +1,7 @@
 package space.devport.utils.item.nbt;
 
 import lombok.experimental.UtilityClass;
+import space.devport.utils.ConsoleOutput;
 import space.devport.utils.version.api.ICompound;
 
 import java.util.HashMap;
@@ -34,6 +35,67 @@ public class TypeUtil {
     }
 
     public final Map<Class<?>, TypeApplier<?>> APPLIERS = new HashMap<Class<?>, TypeApplier<?>>() {{
+        put(Byte.class, new TypeApplier<Byte>() {
+            @Override
+            public void apply(ICompound compound, String key, Object value) {
+                //TODO missing ICompound#withByte
+            }
+
+            @Override
+            public Byte query(ICompound compound, String key) {
+                //TODO
+                return 1;
+            }
+        });
+
+        put(Short.class, new TypeApplier<Short>() {
+            @Override
+            public void apply(ICompound compound, String key, Object value) {
+                compound.withShort(key, (short) value);
+            }
+
+            @Override
+            public Short query(ICompound compound, String key) {
+                return compound.getShort(key);
+            }
+        });
+
+        put(Integer.class, new TypeApplier<Integer>() {
+            @Override
+            public void apply(ICompound compound, String key, Object value) {
+                compound.withInteger(key, (int) value);
+            }
+
+            @Override
+            public Integer query(ICompound compound, String key) {
+                return compound.getInteger(key);
+            }
+        });
+
+        put(Long.class, new TypeApplier<Long>() {
+            @Override
+            public void apply(ICompound compound, String key, Object value) {
+                compound.withLong(key, (long) value);
+            }
+
+            @Override
+            public Long query(ICompound compound, String key) {
+                return compound.getLong(key);
+            }
+        });
+
+        put(Float.class, new TypeApplier<Float>() {
+            @Override
+            public void apply(ICompound compound, String key, Object value) {
+                compound.withFloat(key, (float) value);
+            }
+
+            @Override
+            public Float query(ICompound compound, String key) {
+                return compound.getFloat(key);
+            }
+        });
+
         put(Double.class, new TypeApplier<Double>() {
             @Override
             public void apply(ICompound compound, String key, Object value) {
@@ -45,10 +107,44 @@ public class TypeUtil {
                 return compound.getDouble(key);
             }
         });
+
+        put(byte[].class, new TypeApplier<Byte[]>() {
+            @Override
+            public void apply(ICompound compound, String key, Object value) {
+                //TODO missing ICompound#withByteArray
+            }
+
+            @Override
+            public Byte[] query(ICompound compound, String key) {
+                //TODO
+                return new Byte[0];
+            }
+        });
+
+        put(String.class, new TypeApplier<String>() {
+            @Override
+            public void apply(ICompound compound, String key, Object value) {
+                compound.withString(key, (String) value);
+            }
+
+            @Override
+            public String query(ICompound compound, String key) {
+                return compound.getString(key);
+            }
+        });
+
+        //TODO
+        //TODO missing ICompound#withList
+        //TODO missing ICompound#withIntArray
+        //TODO missing ICompound#withLongArray
     }};
 
     public void setValue(ICompound compound, String key, Object value) {
-        APPLIERS.get(value.getClass()).apply(compound, key, value);
+        TypeApplier<?> applier = APPLIERS.get(value.getClass());
+        if (applier != null)
+            applier.apply(compound, key, value);
+        else
+            ConsoleOutput.getInstance().err("Plugin attempted to save unsupported type " + value.getClass().getSimpleName() + " to NBT.");
     }
 
     public <T> T getValue(ICompound compound, String key, Class<T> clazz) {
